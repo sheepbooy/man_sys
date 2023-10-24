19b294e375b99cc0195cf757d33a26d408fa784c
sheepbooy <1950075421@qq.com>
2023-10-24 17:19:58 +0800

    加入了注册功能

--- management/views.py
+++ management/views.py
@@ -9,7 +9,27 @@ def login(request):
     return render(request, 'login.html')
 
 
+class EmployeesForm(forms.ModelForm):
+    class Meta:
+        model = models.Employees
+        fields = ['name', 'password', 'department', 'gender', 'position', 'work_id', 'status']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs = {'class': 'form-control', 'placeholder': field.label}
+
+
 def register(request):
     """注册"""
-    return render(request, 'register.html')
+    if request.method == 'GET':
+        form = EmployeesForm()
+        return render(request, 'register.html', {'form': form})
 
+    form = EmployeesForm(data=request.POST)
+    if form.is_valid():
+        form.save()
+        return redirect('/login/')
+    else:
+        return render(request, 'register.html', {'form': form})
