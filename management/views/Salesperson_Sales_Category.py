397c99c1824e89763a53afbcb427c45c26db4fad
sheepbooy <1950075421@qq.com>
2023-11-30 01:09:07 +0800

    在之前两个表的基础上增加了使用部门筛选

--- management/views/Salesperson_Sales_Category.py
+++ management/views/Salesperson_Sales_Category.py
@@ -1,33 +1,37 @@
 import json
 from django.core.serializers.json import DjangoJSONEncoder
 from django.shortcuts import render
-from management.models import InternalTradeLedger
-from django.db.models import Sum, F, IntegerField
-from django.db.models.functions import TruncMonth, ExtractYear, Cast
+from management.models import InternalTradeLedger, Reimbursement
+from django.db.models import Sum, F, IntegerField, Value
+from django.db.models.functions import TruncMonth, ExtractYear, Cast, Coalesce
 
 
 def monthly_sales_report(request):
     """202X年-销售部-业务员-月度各品类销售金额"""
+    # 获取部门选项
+    departments = [('all', '全部')] + list(Reimbursement.DEPARTMENT_CHOICES)
+    departments = [name for code, name in departments]
     # 获取内贸部台账中的年份列表
     years = InternalTradeLedger.objects.annotate(
         year_annotate=ExtractYear('sales_month')
     ).values_list('year_annotate', flat=True).distinct()
 
-    # 获取选择的年份参数
+    # 获取选择的年份和部门参数
     selected_year = request.GET.get('year', '')
+    selected_department = request.GET.get('department', 'all')
 
+    # 根据年份和部门进行过滤
+    query = InternalTradeLedger.objects.annotate(year_annotate=ExtractYear('sales_month'))
     if selected_year:
-        # 聚合查询
-        sales_data = InternalTradeLedger.objects \
-            .annotate(year_annotate=ExtractYear('sales_month')) \
-            .filter(year_annotate=selected_year) \
-            .annotate(month_annotate=TruncMonth('sales_month')) \
-            .values('salesperson', 'product_name', 'month_annotate', 'region_department') \
-            .annotate(total_sales=Sum('order_amount')) \
-            .order_by('salesperson', 'product_name', 'month_annotate', 'region_department')
-    else:
-        # 如果没有选择年份，则设置 sales_data 为空列表
-        sales_data = []
+        query = query.filter(year_annotate=selected_year)
+    if selected_department != '全部':
+        query = query.filter(region_department=selected_department)
+
+    sales_data = query \
+        .annotate(month_annotate=TruncMonth('sales_month')) \
+        .values('salesperson', 'product_name', 'month_annotate', 'region_department') \
+        .annotate(total_sales=Sum('order_amount')) \
+        .order_by('salesperson', 'product_name', 'month_annotate', 'region_department')
 
     # 使用 DjangoJSONEncoder 来处理日期
     sales_data_json = json.dumps(list(sales_data), cls=DjangoJSONEncoder)
@@ -37,13 +41,19 @@ def monthly_sales_report(request):
         'sales_data_json': sales_data_json,
         'sales_data': sales_data,
         'selected_year': selected_year,
-        'years': years
+        'selected_department': selected_department,
+        'years': years,
+        'departments': departments
     }
     return render(request, 'monthly_sales_report.html', context)
 
 
 def sales_amount_report(request):
     """202X年-销售部-业务员-月度各品类销售数量"""
+    # 获取部门选项
+    departments = [('all', '全部')] + list(Reimbursement.DEPARTMENT_CHOICES)
+    departments = [name for code, name in departments]
+
     # 获取内贸部台账中的年份列表
     years = InternalTradeLedger.objects.annotate(
         year_annotate=ExtractYear('sales_month')
@@ -51,34 +61,37 @@ def sales_amount_report(request):
 
     # 获取选择的年份参数
     selected_year = request.GET.get('year', '')
+    selected_department = request.GET.get('department', 'all')
 
+    # 根据年份和部门进行过滤
+    query = InternalTradeLedger.objects.annotate(year_annotate=ExtractYear('sales_month'))
     if selected_year:
-        # 聚合查询
-        sales_data = InternalTradeLedger.objects \
-            .annotate(year_annotate=ExtractYear('sales_month')) \
-            .filter(year_annotate=selected_year) \
-            .annotate(month_annotate=TruncMonth('sales_month')) \
-            .values('salesperson', 'product_name', 'month_annotate', 'region_department') \
-            .annotate(
-            total_sales_quantity=Sum(
-                Cast('cash_sales_quantity', IntegerField()) +
-                Cast('receivable_sales_quantity', IntegerField())
-            )
-        ) \
-            .order_by('salesperson', 'product_name', 'month_annotate', 'region_department')
-    else:
-        # 如果没有选择年份，则设置 sales_data 为空列表
-        sales_data = []
+        query = query.filter(year_annotate=selected_year)
+    if selected_department != '全部':
+        query = query.filter(region_department=selected_department)
+
+    # 聚合查询
+    sales_data = query \
+        .annotate(month_annotate=TruncMonth('sales_month')) \
+        .values('salesperson', 'product_name', 'month_annotate', 'region_department') \
+        .annotate(
+        total_sales_quantity=Sum(
+            Coalesce(Cast('cash_sales_quantity', IntegerField()), Value(0)) +
+            Coalesce(Cast('receivable_sales_quantity', IntegerField()), Value(0))
+        )
+    ) \
+        .order_by('salesperson', 'product_name', 'month_annotate', 'region_department')
 
     # 使用 DjangoJSONEncoder 来处理日期
     sales_data_json = json.dumps(list(sales_data), cls=DjangoJSONEncoder)
-
+    # print(sales_data)
     # 渲染页面并传递数据
     context = {
         'sales_data_json': sales_data_json,
         'sales_data': sales_data,
         'selected_year': selected_year,
-        'years': years
+        'selected_department': selected_department,
+        'years': years,
+        'departments': departments
     }
-    print(sales_data)
     return render(request, 'sales_amount_report.html', context)
