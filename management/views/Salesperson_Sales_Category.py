043e0ff06541a39f2d25eb8c1b4e3335e344a966
sheepbooy <1950075421@qq.com>
2023-11-30 14:29:50 +0800

    在部门筛选的基础上，又增加了对前一年增量的展示，不仅在表格中展示，同时在图表中也进行了展示

--- management/views/Salesperson_Sales_Category.py
+++ management/views/Salesperson_Sales_Category.py
@@ -2,7 +2,7 @@ import json
 from django.core.serializers.json import DjangoJSONEncoder
 from django.shortcuts import render
 from management.models import InternalTradeLedger, Reimbursement
-from django.db.models import Sum, F, IntegerField, Value
+from django.db.models import Sum, F, IntegerField, Value, FloatField
 from django.db.models.functions import TruncMonth, ExtractYear, Cast, Coalesce
 
 
@@ -11,13 +11,14 @@ def monthly_sales_report(request):
     # 获取部门选项
     departments = [('all', '全部')] + list(Reimbursement.DEPARTMENT_CHOICES)
     departments = [name for code, name in departments]
+
     # 获取内贸部台账中的年份列表
     years = InternalTradeLedger.objects.annotate(
         year_annotate=ExtractYear('sales_month')
     ).values_list('year_annotate', flat=True).distinct()
 
     # 获取选择的年份和部门参数
-    selected_year = request.GET.get('year', '')
+    selected_year = request.GET.get('year', years[0])
     selected_department = request.GET.get('department', 'all')
 
     # 根据年份和部门进行过滤
@@ -27,23 +28,16 @@ def monthly_sales_report(request):
     if selected_department != '全部':
         query = query.filter(region_department=selected_department)
 
-    sales_data = query \
-        .annotate(month_annotate=TruncMonth('sales_month')) \
-        .values('salesperson', 'product_name', 'month_annotate', 'region_department') \
-        .annotate(total_sales=Sum('order_amount')) \
-        .order_by('salesperson', 'product_name', 'month_annotate', 'region_department')
-
-    # 使用 DjangoJSONEncoder 来处理日期
-    sales_data_json = json.dumps(list(sales_data), cls=DjangoJSONEncoder)
-
     # 获取前一年的年份
     previous_year = str(int(selected_year) - 1) if selected_year else ''
 
-    # 查询当前选定年份的销售数据
+    # 在查询中处理可能的空字符串
     current_year_sales = query \
         .annotate(month_annotate=TruncMonth('sales_month')) \
         .values('salesperson', 'product_name', 'month_annotate', 'region_department') \
-        .annotate(total_sales=Sum('order_amount')) \
+        .annotate(total_sales=Sum(
+        Coalesce('order_amount', Value(0), output_field=FloatField())
+    )) \
         .order_by('salesperson', 'product_name', 'month_annotate', 'region_department')
 
     # 查询前一年的销售数据
@@ -79,7 +73,6 @@ def monthly_sales_report(request):
         'departments': departments
     }
 
-    print(sales_data)
     return render(request, 'monthly_sales_report.html', context)
 
 
@@ -95,7 +88,7 @@ def sales_amount_report(request):
     ).values_list('year_annotate', flat=True).distinct()
 
     # 获取选择的年份参数
-    selected_year = request.GET.get('year', '')
+    selected_year = request.GET.get('year', years[0])
     selected_department = request.GET.get('department', 'all')
 
     # 根据年份和部门进行过滤
@@ -105,8 +98,11 @@ def sales_amount_report(request):
     if selected_department != '全部':
         query = query.filter(region_department=selected_department)
 
-    # 聚合查询
-    sales_data = query \
+    # 获取前一年的年份
+    previous_year = str(int(selected_year) - 1) if selected_year else ''
+
+    # 查询当前选定年份的销售数据
+    current_sales_amount = query \
         .annotate(month_annotate=TruncMonth('sales_month')) \
         .values('salesperson', 'product_name', 'month_annotate', 'region_department') \
         .annotate(
@@ -117,13 +113,39 @@ def sales_amount_report(request):
     ) \
         .order_by('salesperson', 'product_name', 'month_annotate', 'region_department')
 
+    # 查询前一年的销售数据
+    previous_sales_amount = InternalTradeLedger.objects.annotate(
+        year_annotate=ExtractYear('sales_month')
+    ).filter(year_annotate=previous_year) \
+        .annotate(month_annotate=TruncMonth('sales_month')) \
+        .values('salesperson', 'product_name', 'month_annotate', 'region_department') \
+        .annotate(
+        total_sales_quantity=Sum(
+            Coalesce(Cast('cash_sales_quantity', IntegerField()), Value(0)) +
+            Coalesce(Cast('receivable_sales_quantity', IntegerField()), Value(0))
+        )
+    ) \
+        .order_by('salesperson', 'product_name', 'month_annotate', 'region_department')
+
+    # 将前一年的销售数据转换为字典以便快速查找
+    previous_sales_amount_dict = {
+        (item['salesperson'], item['product_name'], item['month_annotate'], item['region_department']): item[
+            'total_sales_quantity']
+        for item in previous_sales_amount}
+
+    # 计算增量
+    for item in current_sales_amount:
+        prev_sales = previous_sales_amount_dict.get(
+            (item['salesperson'], item['product_name'], item['month_annotate'], item['region_department']), 0)
+        item['sales_increment'] = item['total_sales_quantity'] - prev_sales
+
     # 使用 DjangoJSONEncoder 来处理日期
-    sales_data_json = json.dumps(list(sales_data), cls=DjangoJSONEncoder)
-    # print(sales_data)
+    sales_data_json = json.dumps(list(current_sales_amount), cls=DjangoJSONEncoder)
+
     # 渲染页面并传递数据
     context = {
         'sales_data_json': sales_data_json,
-        'sales_data': sales_data,
+        'sales_data': current_sales_amount,
         'selected_year': selected_year,
         'selected_department': selected_department,
         'years': years,
